f10b0f87e70045fe1b693a954b003f556ad57a6a
Buff9 <yura.korovickiy9583@gmail.com>
2022-05-18 19:05:37 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from '@angular/common/http';
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    CommentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
